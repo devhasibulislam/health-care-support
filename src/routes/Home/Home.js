ebfb687cfbacbd1a4df66cf640ed8ace2699a979
hasibulislam999 <hasib143sl@gmail.com>
2022-04-14 15:54:32 +0600

    route completion process

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
     return (
         <section className='mb-5'>
             {/* health care support */}
-            <section className='d-flex align-items-center justify-content-center p-5'>
+            <section className='d-flex align-items-center justify-content-center p-5 container mx-auto flex-column flex-lg-row'>
                 <div>
                     <h1 className='display-1'>Health Care & Support</h1>
                     <p className='text-muted'>Care Your Health & Lead A Happy Life With Your Family.</p>
@@ -18,12 +18,12 @@ const Home = () => {
                     </Button>
                 </div>
                 <div>
-                    <img src="images/banner.svg" alt="hero_image" className='mw-100'/>
+                    <img src="images/banner.svg" alt="hero_image" className='mw-100' />
                 </div>
             </section>
             <section className="container mx-auto">
                 {/* hello doctor */}
-                <section className='d-flex align-items-center justify-content-center p-5 bg-white mb-5 shadow'>
+                <section className='d-flex align-items-center justify-content-center p-5 bg-white mb-5 shadow flex-lg-row flex-column'>
                     <div>
                         <img src="images/aboutus.svg" alt="about_us" className='mw-100' />
                     </div>
@@ -49,6 +49,33 @@ const Home = () => {
                             ></Service>)
                         }
                     </div>
+                </section>
+                {/* customer conviction */}
+                <section className='mt-5'>
+                    <h2 className='d-inline-block border-bottom border-3 border-primary mb-4'>What Our Customer Say About Us</h2>
+                    <div className='row g-5'>
+                        <div className="col-lg-6 p-3">
+                            <p>
+                                Health Care Support were all very friendly and helpful. I especially loved how Drs really took his time to explain my conditions with me as well as my treatment options. I had a great visit and the doctor's demeanor has really put me at ease so I highly recommend this clinic.
+                            </p>
+                            <h4 className='text-primary mb-0'>Sadia Dhira</h4>
+                            <h5 className='text-muted'><small>Designer</small></h5>
+                        </div>
+                        <div className="col p-3 bg-white shadow">
+                            <p>
+                                From his excellent treatment, curiosity, investigative mind and ability to connect, you know where you stand immediately and what next steps look like. Attention doctors if you want a masterclass in watching a doctor bring medical knowledge and build rapport so that message is heard by patient.
+                            </p>
+                            <h4 className='text-primary mb-0'>Sabbir Hossain</h4>
+                            <h5 className='text-muted'><small>Developer</small></h5>
+                        </div>
+                    </div>
+                </section>
+                {/* need a doctor */}
+                <section className='mt-5 p-5 d-flex justify-content-between bg-white shadow'>
+                    <h2 className='mb-0'>Don't feel better, Need a doctor?</h2>
+                    <button className='btn btn-primary rounded-pill px-5'>
+                        <Link to={'/register'} style={{color: 'white', textDecoration: 'none'}}>Register Now</Link>
+                    </button>
                 </section>
             </section>
         </section>
--- src/routes/Service/Service.js
+++ src/routes/Service/Service.js
@@ -3,13 +3,14 @@ import React from 'react';
 const Service = ({ service }) => {
     const { name, description, photo } = service;
     return (
-        <section className='col-4 d-flex'>
+        <section className='col-lg-4 col-md-6 d-flex'>
             <div>
                 <img src={photo} alt="service_logo" className='mw-100' />
             </div>
             <div className='ms-2'>
                 <h5>{name}</h5>
                 <p>{description}</p>
+                <button className='btn btn-outline-primary rounded-pill'>Take this service</button>
             </div>
         </section>
     );
